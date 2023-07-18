2110276638ba3b9ad56e6737ab72eb31311838c9
qasemzoubii <qasem.zoubi2000@gmail.com>
2023-07-18 09:03:02 +0300

    commit

--- 17-june-2023/Task.js
+++ 17-june-2023/Task.js
@@ -1,25 +1,25 @@
-let btn = document.querySelector("button")
-btn.addEventListener("click",function(){
-let tbl= document.querySelector("table")
-let x = new XMLHttpRequest()
-x.open('GET','Task.json') 
-x.onload= function () {
-let jasondata =JSON.parse(x.responseText)
-for (let i = 0; i < jasondata.length; i++) {
-   let row = document.createElement("tr")
-   tbl.appendChild(row)
-   for (const key in jasondata[i]) {
-if(key==="image"){
-    let td = document.createElement("td")
-    td.innerHTML = `<img src="${jasondata[i] [`${key}`]}" alt="">`
-    row.appendChild(td)
-}
-else{
-let td = document.createElement("td")
-row.appendChild(td)
-td.innerHTML=jasondata[i] [`${key}`]
-}
-}
-}
-}
-x.send()})
+let btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  let tbl = document.querySelector("table");
+  let x = new XMLHttpRequest();
+  x.open("GET", "Task.json");
+  x.onload = function () {
+    let jsondata = JSON.parse(x.responseText);
+    for (let i = 0; i < jsondata.length; i++) {
+      let row = document.createElement("tr");
+      tbl.appendChild(row);
+      for (const key in jsondata[i]) {
+        if (key === "image") {
+          let td = document.createElement("td");
+          td.innerHTML = `<img src="${jsondata[i][`${key}`]}" alt="">`;
+          row.appendChild(td);
+        } else {
+          let td = document.createElement("td");
+          row.appendChild(td);
+          td.innerHTML = jsondata[i][`${key}`];
+        }
+      }
+    }
+  };
+  x.send();
+});
